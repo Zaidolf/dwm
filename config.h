ea3ed9c071dadbf3dd8df4aecf76999d3976f3c8
Zaidolf <hinfluenced@gmail.com>
2024-09-10 23:38:42 +0300

    Update config.h

--- config.h
+++ config.h
@@ -60,14 +60,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *scrshot[] = { "flameshot gui", NULL};
+static const char *scrshot[] = { "flameshot", "gui", NULL};
 
 #include "shiftview.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY, 			XK_d,      spawn, 	   {.v = dmenucmd }},
 	{ MODKEY,			XK_Return,  spawn,    	   {.v = termcmd }},
-	{ 0,    			PrintScreenDWM, spawn,     {.v = scrshot }},
+	{ 0,    			XK_Print, spawn,     {.v = scrshot }},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
